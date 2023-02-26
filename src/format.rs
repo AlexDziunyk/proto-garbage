51e45f2cdaaa1e6c1f5d77b59bfa59fd0a7ac9e3
Konstantin Teslenko <kostya5779025@gmail.com>
2023-02-26 03:48:35 +0200

    Add format_and_write function

--- src/format.rs
+++ src/format.rs
@@ -1,10 +1,17 @@
 use std::{
+    fs::write,
     io::Write,
+    path::Path,
     process::{Command, Stdio},
     thread,
 };
 
 use anyhow::{anyhow, Result};
+
+pub fn format_and_write<P: AsRef<Path>>(path: P, code: &str) -> Result<()> {
+    let formatted = format_code(code).unwrap_or(code.to_owned());
+    Ok(write(path, formatted)?)
+}
 
 pub fn format_code(code: &str) -> Result<String> {
     let mut clang_format = Command::new("clang-format")
